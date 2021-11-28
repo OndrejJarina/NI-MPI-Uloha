3c8573e0cf0c324c8f08373db7d9bf653b722671
Ondrej Jarina <>
2021-11-28 19:16:45 +0100

    added code comments

--- gauss_seidel_metoda.m
+++ gauss_seidel_metoda.m
@@ -1,19 +1,27 @@
 function [x] = gauss_seidel_metoda(gamma)
+% skript sa spusti zavolanim metody s parametrom gamma, podla ktoreho 
+% je vytvorena vstupna matica A
 
 format longG
 dimension = 20;
 
+% pociatocne x je rovne nulovemu vektoru
 x = zeros(dimension,1);
 
+% vytvorenie matice A - tri zlozky (Ls - horna trojuholnikova matica
+% Us - dolna trojuholnikova matica, D - diagonalna matica)
 Ls = diag(ones(1,(dimension-1))*(-1), 1);
 Us = diag(ones(1, (dimension-1))*(-1), -1);
 D = diag(ones(1, dimension)*gamma);
+A = (Ls + Us + D);
 
+% vytvorenie vektora b
 b = ones(dimension,1)*(gamma - 2);
 b(1) = b(1)+1;
 b(dimension) = b(dimension)+1;
-A = (Ls + Us + D);
 
+% kontrola podmienky konvergencie
+% matica A musi byt symetricka a pozitivne definitna
 if (~issymmetric(A))
     error("Matrix is not symetric!");
 elseif (~all(eig(A) > 0)) 
@@ -22,7 +30,9 @@ end
 
 iterations = 0;
 
+%algoritmus gauss-seidelovej metody
 while (norm(A*x - b) / norm(b)) >= (10 ^-6)
+    % vypocet iteracie neznameho vektora x
     x_k = (D+Ls)\((D+Ls-A)*x + b);
     x = double(x_k);
     iterations = iterations+1;
--- jacobi_metoda.m
+++ jacobi_metoda.m
@@ -1,30 +1,40 @@
 function [x] = jacobi_metoda(gamma)
+% skript sa spusti zavolanim metody s parametrom gamma, podla ktoreho 
+% je vytvorena vstupna matica A
 
+% nastavenie formatu zobrazovania cisel
 format longG
 dimension = 20;
 
+% pociatocne x je rovne nulovemu vektoru 
 x = zeros(dimension,1);
 
+% vytvorenie matice A - tri zlozky (Ls - horna trojuholnikova matica
+% Us - dolna trojuholnikova matica, D - diagonalna matica)
 Ls = diag(ones(1,(dimension-1))*(-1), 1);
 Us = diag(ones(1, (dimension-1))*(-1), -1);
 D = diag(ones(1, dimension)*gamma);
+A = (Ls + Us + D);
 
+% vektor b
 b = ones(dimension,1)*(gamma - 2);
 b(1) = b(1)+1;
 b(dimension) = b(dimension)+1;
-A = (Ls + Us + D);
-LU = Ls+Us;
 
-rowsSum = sum(abs(LU), 1);
-columnsSum = sum(abs(LU), 2);
+% kontrola podmienky konvergencie
+% podmienkou konvergencie je, ci matica je ostro diagonalne dominantna
+rowsSum = sum(abs(Ls+Us), 1);
+columnsSum = sum(abs(Ls+Us), 2);
 
 if (all(rowsSum >= sum(abs(D))) || all(columnsSum.' >= sum(abs(D)))) 
     error("Matrix is not diagonally dominant!"); 
 end
 
 iterations = 0;
-
+% algoritmus jacobiho metody
+% podmienka while = kontrola zastavovacieho kriteria
 while (norm(A*x - b) / norm(b)) >= (10 ^-6)
+    % vypocet iteracie neznameho vektora x
     x_k = D\(b-(Ls+Us)*x);
     x = double(x_k);
     iterations = iterations+1;
